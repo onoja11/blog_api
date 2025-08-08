88ed33bde50f51af42c45f0a71d16bea8b146a35
Veritas <ochigbogodswill868@gmail.com>
2025-08-08 00:04:14 -0700

    authentication with test case added

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 Route::apiResource('posts', PostController::class);
 Route::apiResource('users', UserController::class);
+
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum');
