035df7cb59a6328358cb9e020615b0f4a5df1329
Veritas <ochigbogodswill868@gmail.com>
2025-08-08 23:18:35 -0700

    added middleware to posts route (auth:sanctum)

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::apiResource('posts', PostController::class);
+Route::apiResource('posts', PostController::class)->only(['index','show']);
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum')->only(['store','update','destroy']);
 Route::apiResource('users', UserController::class);
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
